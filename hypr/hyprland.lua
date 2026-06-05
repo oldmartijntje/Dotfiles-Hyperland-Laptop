573a3e5349ded0bc5404eacd278a56392c3bd370
oldmartijntje <oldmartijntje@gmail.com>
2026-06-05 12:45:01 +0200

    empty the config

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -5,24 +5,3 @@ require("modules.autostart")
 require("modules.perms")
 require("modules.input")
 require("modules.design")
-
--- This is an example Hyprland Lua config file.
--- Refer to the wiki for more information.
--- https://wiki.hypr.land/Configuring/Start/
-
--- Please note not all available settings / options are set here.
--- For a full list, see the wiki
-
--- You can (and should!!) split this configuration into multiple files
--- Create your files separately and then require them like this:
--- require("myColors")
-
-
----------------------
----- MY PROGRAMS ----
----------------------
-
--- Set programs that you use
-local terminal    = "kitty"
-local fileManager = "dolphin"
-local menu        = "hyprlauncher"
