1220905ebe27969b96b83d975189ffcf6acd010d
oldmartijntje <oldmartijntje@gmail.com>
2026-06-07 17:46:32 +0200

    add a wallpaper

--- hypr/modules/autostart.lua
+++ hypr/modules/autostart.lua
@@ -6,9 +6,14 @@
 
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
---
--- hl.on("hyprland.start", function () 
---   hl.exec_cmd(terminal)
---   hl.exec_cmd("nm-applet")
---   hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
+
+-- Set programs that you use
+local terminal    = "kitty"
+local fileManager = "dolphin"
+local menu        = "hyprlauncher"
+
+hl.on("hyprland.start", function () 
+  hl.exec_cmd(terminal)
+  hl.exec_cmd("nm-applet")
+  hl.exec_cmd("waybar & hyprpaper & flatpak run app.zen_browser.zen")
+end)
